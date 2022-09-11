968a35d7748184c452e27a1f71cef3aaf2922775
Anna7Lov <anna17lov@gmail.com>
2022-09-11 23:45:36 +0300

    cart added

--- project-furniking/js/scripts.js
+++ project-furniking/js/scripts.js
@@ -1,54 +1,47 @@
 const sideMenuButton = document.querySelector('.header__catalog-button');
-const verticalMenu = document.querySelector('.categories-menu');
-sideMenuButton.onClick = handleSideMenuButtonClick;
+const categoriesMenu = document.querySelector('.categories-menu');
 sideMenuButton.addEventListener('click', handleSideMenuButtonClick);
 
 function handleSideMenuButtonClick() {
-  verticalMenu.classList.toggle('closed');  
+ categoriesMenu.classList.toggle('closed');  
 }
 
 const menuButton = document.querySelector('.header__menu');
 const mobileMenu = document.querySelector('.header__middle-content');
-menuButton.onClick = handleMenuButtonClick;
 menuButton.addEventListener('click', handleMenuButtonClick);
 
 function handleMenuButtonClick() {
   mobileMenu.classList.toggle('active');
-  verticalMenu.classList.remove('active');
-  verticalMenu.classList.add('closed');
+ categoriesMenu.classList.remove('active');
+ categoriesMenu.classList.add('closed');
   sideMenuButton.addEventListener('click', handleAdaptiveMenuButtonClick);
   function handleAdaptiveMenuButtonClick() {
-    verticalMenu.classList.toggle('active');  
+   categoriesMenu.classList.toggle('active');  
   }
 }
 
-
-
 const sofaPointer = document.querySelector('.sofa');
 const sofaProduct = document.querySelector('.sofa-product');
-sofaPointer.onClick = handleSofaPointerClick;
 sofaPointer.addEventListener('click', handleSofaPointerClick);
 
 function handleSofaPointerClick() {
-  sofaProduct.classList.toggle('active');
+  sofaProduct.classList.toggle('active-product');
 }
 
 const tablePointer = document.querySelector('.table');
 const tableProduct = document.querySelector('.table-product');
-tablePointer.onClick = handleTablePointerClick;
 tablePointer.addEventListener('click', handleTablePointerClick);
 
 function handleTablePointerClick() {
-  tableProduct.classList.toggle('active');
+  tableProduct.classList.toggle('active-product');
 }
 
 const coffeeTablePointer = document.querySelector('.coffee-table');
 const coffeeTableProduct = document.querySelector('.coffee-table-product');
-coffeeTablePointer.onClick = handleCoffeeTablePointerClick;
 coffeeTablePointer.addEventListener('click', handleCoffeeTablePointerClick);
 
 function handleCoffeeTablePointerClick() {
-  coffeeTableProduct.classList.toggle('active');
+  coffeeTableProduct.classList.toggle('active-product');
 }
 
 function updateTextInput(val) {
